f57468c55af6ec92afea693bdf00119ed5111b0a
Eolaes <eolaes@hennypenny.com>
2025-10-02 21:43:19 +0800

    Update to add 2 more test: 1 error test and 1 normal test

--- TDD/can/can_test.cpp
+++ TDD/can/can_test.cpp
@@ -19,7 +19,22 @@ TEST_GROUP(can_test_group)
     }
 };
 
+TEST(can_test_group, can_read_from_bus)
+{
+    char buffer[] = "Hello";
+    int result = can_read_from_bus(buffer, 1);
+    CHECK_EQUAL(0, result);
+}
+
 TEST(can_test_group, can_clear_bus)
 {
     can_clear_bus();
 }
+
+TEST(can_test_group, can_write_to_bus)
+{
+    char buffer[] = "Hello";
+    int result = can_write_to_bus(buffer, 1);
+    CHECK_EQUAL(0, result);
+}
+
