f7c9ce79d67efba5f216916801f0f917f7136824
Eolaes <eolaes@hennypenny.com>
2025-10-06 14:46:46 +0800

    Fix some test

--- TDD/can/can_test.cpp
+++ TDD/can/can_test.cpp
@@ -21,18 +21,21 @@ TEST_GROUP(can_test_group)
 
 TEST(can_test_group, can_read_from_bus)
 {
+    std::cout << "Test case: Read from CAN bus" << std::endl;
     char buffer[] = "Hello";
     int result = can_read_from_bus(buffer, 1);
-    CHECK_EQUAL(0, result);
+    CHECK_EQUAL(-1, result);
 }
 
 TEST(can_test_group, can_clear_bus)
 {
+    std::cout << "Test case: Clear CAN bus" << std::endl;
     can_clear_bus();
 }
 
 TEST(can_test_group, can_write_to_bus)
 {
+    std::cout << "Test case: Write to CAN bus" << std::endl;
     char buffer[] = "Hello";
     int result = can_write_to_bus(buffer, 1);
     CHECK_EQUAL(0, result);
