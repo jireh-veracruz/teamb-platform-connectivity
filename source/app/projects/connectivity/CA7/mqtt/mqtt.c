3060490bc151dd28b39fa778744aedd744ee13d4
Jireh Vera Cruz <jcruz@hennypenny.com>
2025-10-20 13:45:05 +0800

    Test memory leak

--- source/app/projects/connectivity/CA7/mqtt/mqtt.c
+++ source/app/projects/connectivity/CA7/mqtt/mqtt.c
@@ -4,8 +4,11 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "mqtt.h"
 
 void print_message(char *message) {
     printf("%s\n", message);
+    char *leaked_buffer = malloc(20);  // Creates intentional leak
+    // Intentionally no free()
 }
